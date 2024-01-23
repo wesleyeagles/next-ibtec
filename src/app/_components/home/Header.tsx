d61921387a5033cfe9300b1dd3acea4cea11077f
Wesley <wesley.carvalho@crearesistemas.com.br>
2024-01-23 11:26:31 -0300

    remove video from mobile

--- src/app/_components/home/Header.tsx
+++ src/app/_components/home/Header.tsx
@@ -9,9 +9,11 @@ import { Carousel } from "primereact/carousel";
 import StraightIcon from '@mui/icons-material/Straight';        
 
 import dynamic from 'next/dynamic'
+import { useMediaQuery } from "@mui/material";
 const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 export default function Header() {
+    const isMedia600px = useMediaQuery("(max-width: 600px)");
     const intl = useIntl();
 
     const styles = {
@@ -19,6 +21,7 @@ export default function Header() {
             style: {
                 position: "absolute",
                 right: "15px",
+                display: isMedia600px ? "none" : "block",
             },
         },
         nextButtonIcon: {
@@ -36,6 +39,7 @@ export default function Header() {
                 position: "absolute",
                 left: "0px",
                 zIndex: "999999",
+                display: isMedia600px ? "none" : "block",
             },
         },
         indicators: {
@@ -75,7 +79,27 @@ export default function Header() {
         }
     ]
 
+    const itemsMobile = [
+        {
+            type: "html",
+            content: (
+                <div className="home-banner">
+                    <Container>
+                        <div className="flex flex-col justify-center h-full pt-14 sm:pt-16">
+                            <h1 className="text-[1.75rem] sm:text-4xl md:text-5xl md:text-5xl font-bold sm:w-[420px] md:w-[550px] md:leading-tight">{}{intl.formatMessage({ id: "h1_header_1" })}</h1>
+                            <p className="text-sm sm:text-xl md:leading-relaxed sm:w-[420px] md:w-[450px] mt-4 mb-8 sm:mb-20 sm:mt-8">{intl.formatMessage({ id: "p_header" })}</p>
+                            <InputButton href={`/${intl.locale}/contact`} />
+                            <div className="border border-gray-600 w-max p-2 sm:p-5 rounded-full rotate-180 mt-16 sm:mt-32 cursor-pointer">
+                                <StraightIcon className="animate-bounce" fontSize="large" />
+                            </div>
+                        </div>
+                    </Container>
+                </div>
+            )
+        }
+    ]
+
     return (
-        <Carousel itemTemplate={itemTemplate} pt={styles} value={items} />
+        <Carousel itemTemplate={itemTemplate} pt={styles} value={isMedia600px ? itemsMobile : items} />
     )
 }
